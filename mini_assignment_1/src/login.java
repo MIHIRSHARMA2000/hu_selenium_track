f6359674f6dc66a508e01ef0be42a8d2a3fe149c
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-03-23 12:52:13 +0530

    updated

--- mini_assignment_1/src/login.java
+++ mini_assignment_1/src/login.java
@@ -9,13 +9,21 @@ public class login {
         WebDriver driver= new ChromeDriver();
         driver.get(" https://www.phptravels.net/hotels");
        WebElement  logo=  driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[2]/div/div/div/div/div[1]/a/img"));
+
         WebElement home= driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[2]/div/div/div/div/div[2]/nav/ul/li[1]/a"));
+        //home.click();
       WebElement flight=  driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[2]/div/div/div/div/div[2]/nav/ul/li[3]/a"));
+      //flight.click();
         WebElement tours=driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[2]/div/div/div/div/div[2]/nav/ul/li[4]/a"));
+        //tours.click();
         WebElement company= driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[2]/div/div/div/div/div[2]/nav/ul/li[8]/a"));
+        //company.click();
        WebElement signup= driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[1]/div/div/div[2]/div/div/a[1]"));
+       //signup.click();
         WebElement login = driver.findElement(By.xpath("//*[@id=\"fadein\"]/header/div[1]/div/div/div[2]/div/div/a[2]"));
+        //login.click();
        WebElement agent= driver.findElement(By.xpath("//*[@id=\"agents\"]"));
+       //agent.click();
 
 
 
