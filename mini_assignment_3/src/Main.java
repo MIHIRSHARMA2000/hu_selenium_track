d2cbf5393af34114fa50c8ba1122e5212b36ed0c
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-03-24 17:07:24 +0530

    updated_solution_1

--- mini_assignment_3/src/Main.java
+++ mini_assignment_3/src/Main.java
@@ -1,19 +1,20 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\mihirsharma\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         //framehandling(driver);
-        DragAndDrop(driver);
+        //DragAndDrop(driver);
         //alerthandling(driver);
-        //scenatio4(driver);
+        scenario4(driver);
     }
     public static void framehandling(WebDriver driver)
     {
@@ -40,8 +41,7 @@ public class Main {
         builder.dragAndDropBy(driver.findElement(By.xpath("//div[@id=\"draggable\"]")),153,26).perform();
         driver.quit();
     }
-    public static void alerthandling(WebDriver driver)
-    {
+    public static void alerthandling(WebDriver driver)  {
         driver.get("https://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
         WebElement javaAlert= driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[29]/a"));
@@ -50,6 +50,7 @@ public class Main {
         jsprompt.click();
         String getalert= driver.switchTo().alert().getText();
         System.out.println(getalert);
+
         driver. switchTo(). alert(). sendKeys("Test");
          driver.switchTo().alert().accept();
          WebElement result = driver.findElement(By.xpath("//*[@id=\"result\"]"));
@@ -63,32 +64,77 @@ public class Main {
          driver.close();;
     }
 
-    public static void scenatio4(WebDriver driver)
-    {
+    public static void scenario4(WebDriver driver) throws InterruptedException {
         driver.get("https://www.goibibo.com/");
-        WebElement roundTrip = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/ul/li[2]/span[1]"));
-        roundTrip.click();
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/p")).sendKeys("New York");
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[2]/div/div/p")).sendKeys("Seattle");
-        Select object  = new Select(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div/p[1]/span")));
-        WebElement navigator = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/span[2]"));
-        navigator.click();;
-        navigator.click();
-        WebElement DepartureDate = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div[1]/div[3]/div[4]/div[6]"));
-        DepartureDate.click();
-        WebElement done = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/span[2]"));
+        Actions action = new Actions(driver);
+
+        WebElement roundTripButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/ul/li[2]/span[1]"));
+        roundTripButton.click();
+        WebElement From = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/p"));
+        From.click();
+        WebElement FromText = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[1]/div/div[2]/div/input"));
+        FromText.click();
+        FromText.sendKeys("New York (NYC)");
+        Thread.sleep(2000);
+
+        WebElement selectFrom = driver.findElement(By.xpath("//*[@id=\"autoSuggest-list\"]/li[1]"));
+        action.click(selectFrom).build().perform();
+
+        Thread.sleep(2000);
+
+        WebElement toText = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/input[1]"));
+
+        toText.sendKeys("Seattle (SEA)");
+        Thread.sleep(2000);
+        WebElement tofrom = driver.findElement(By.xpath("//*[@id=\"autoSuggest-list\"]/li[1]/div/div[1]/div/p[1]/span[1]"));
+        action.click(tofrom).build().perform();
+        while(!driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div")).getText().contains("June")){
+            driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[1]/span[2]")).click();
+        }
+        //24 June date is selected
+        WebElement date24=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[4]/div[6]"));
+        date24.click();
+        Thread.sleep(1500);
+
+        //Click on done button
+        WebElement done=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/span[2]"));
         done.click();
-        WebElement navigator2= driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[4]/div/p[1]/span"));
-        navigator2.click();
-        WebElement ReturnDate = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div[6]"));
-        ReturnDate.click();
-        WebElement done1 = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[3]/span[2]"));
+        Thread.sleep(1500);
+
+        //click on done button
+        WebElement done1=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[5]/div[2]/div[3]/a[2]"));
         done1.click();
-        WebElement confirm = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[5]/div[2]/div[3]/a[2]"));
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[3]/span")).click();
-        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/button")).click();
+        Thread.sleep(1500);
+        WebElement returndate=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[4]/div/p"));
+        returndate.click();
+        Thread.sleep(1500);
 
+        //8 July date is selected
+        WebElement date8=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div[2]/div[3]/div[2]/div[6]/p"));
+        date8.click();
+        Thread.sleep(1500);
 
+        WebElement Done=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[4]/div[2]/div[3]/span[2]"));
+        Done.click();
+        Thread.sleep(1500);
+
+        WebElement done2=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[1]/div[5]/div[2]/div[3]/a[2]"));
+        done2.click();
+        Thread.sleep(1500);
+
+        //Click on SearchFlight Button
+        WebElement SearchFlight=driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[3]/span"));
+        SearchFlight.click();
+        System.out.println("search operation is completed");
+        Thread.sleep(1500);
+
+        Take Screenshot and save as jpg
+        File screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(screenshot,new File("C:\\Users\\mihirsharma\\hu_selenium_track\\mini_assignment_3"));
+        Thread.sleep(2000);*/
+        WebElement Book=driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[2]/div/div[2]/div/div[2]/div[2]/div[2]/div/button"));
+        Book.click();
+        driver.quit();
 
     }
 }
